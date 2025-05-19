312d35f70b2dff817f6df5cb487fcf3a2fca3edf
ITac00 <126593798+ITac00@users.noreply.github.com>
2025-05-19 14:27:48 +0200

    Dodaj pliki projektów.

---
+++ ServiceSdkDemo.Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Devices;
+using ServiceSdkDemo.Console;
+using ServiceSdkDemo.Lib;
+
+string serviceConnectionString = "HostName=Zaliczenie.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=Y3seimezGWaCt4rG/NBxoqwdvf2UP90KQAIoTHlyUNQ=";
+
+using var serviceClient = ServiceClient.CreateFromConnectionString(serviceConnectionString);
+using var registryManager = RegistryManager.CreateFromConnectionString(serviceConnectionString);
+
+var manager = new IoTHubManager(serviceClient, registryManager);
+
+int input;
+do
+{
+    FeatureSelector.PrintMenu();
+    input = FeatureSelector.ReadInput();
+    await FeatureSelector.Execute(input, manager);
+} while (input != 0);
